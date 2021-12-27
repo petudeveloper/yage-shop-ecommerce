867e82be72307284ec7258d85cf5fcdd5946d397
petumazo <davidalvarezmazzo@gmail.com>
2021-12-27 17:33:10 -0500

    Setup mongoDB connection.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 var express = require('express');
+var cors = require('cors')
 var app = express();
+const mongoose = require('mongoose');
 
-app.get('/', function (req, res) {
-  res.send('Inside the server')
-})
+mongoose.connect(process.env.MDB_CONNECTION_STRING)
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.error(err));
 
-app.listen(process.env.PORT, () => {
-  console.log(`server running on ${process.env.PORT}`)
-});
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('cat created successfully'));
+
+// app.use(cors());
+
+// app.get('/', function (req, res) {
+//   res.send('Inside the server')
+// })
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`server running on ${process.env.PORT}`)
+// });
