e1342f41e0a8fe3f2e8df7659f8e6c259f3d73b4
petudeveloper <davidalvarezmazzo@mail.com>
2022-01-15 23:54:05 -0500

    Create user routes.

---
+++ server/api/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { createUser, loginUser } = require('../controllers/userController');
+
+// users#create POST /users
+router.post('/users', createUser);
+
+// users#login POST /users/login
+router.post('/users/login', loginUser);
